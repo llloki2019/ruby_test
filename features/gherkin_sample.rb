3530c325fe39506aac63234da39050dc96eab01d
Xiao Lu Qiu <xiao-lu.qiu@microfocus.com>
2021-10-20 16:14:48 +0800

    tech: all passed

--- features/gherkin_sample.rb
+++ features/gherkin_sample.rb
@@ -1,68 +1,52 @@
 require 'minitest'
 Given('a global administrator named {string}') do |string|
-  puts 'background given: ' + string
 end
 
 Given('a blog named {string}') do |string|
-  puts 'background given: ' + string
 end
 
 Given('a customer named {string}') do |string|
-  puts 'background given: ' + string
 end
 
 Given('the following people exist:') do |table|
   # table is a Cucumber::MultilineArgument::DataTable
-#   raise 'error occurs'
-  puts 'Given: ' + table.to_s
+  raise 'error occurs'
     # pending 'pending implementation'
     # assert(false, "This was expected to be true")
 end
 
 Given('some precondition 1') do
-  puts 'Given: some precondition 1'
 end
 
 When('some action by the actor') do
-  puts 'When: some action by the actor'
 end
 
 When('some other action') do
-  puts 'When: some other action'
 end
 
 Then('some testable outcome is achieved') do
-  puts 'Then: some testable outcome is achieved'
 end
 
 Then('something else we can check happens too') do
-  puts 'Then: something else we can check happens too'
 end
 
 Given('some precondition') do
-  puts 'Given: some precondition'
 end
 
 Given('some other precondition with doc string') do |doc_string|
-  puts 'Given: ' + doc_string
 end
 
 When('yet another action') do
-  puts 'When: yet another action'
 end
 
 Then('I don\'t see something else') do
-  puts 'Then: I don\'t see something else'
 end
 
 Given('the cow weighs {int} kg') do |int|
-  puts 'Given: ' + int.to_s + 'kg'
 end
 
 When('we calculate the feeding requirements') do
-  puts 'When: we calculate the feeding requirements'
 end
 
-# Then('the energy should be {int} MJ') do |int|
-#   puts 'Then:' + int.to_s + 'MJ'
-# end
+Then('the energy should be {int} MJ') do |int|
+end
